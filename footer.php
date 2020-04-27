c732ee05b02d8fc6e636f2b5d3e7ca4df55d26d1
g.tsenis@gmail.com <g.tsenis@gmail.com>
2020-04-27 16:55:28 +0300

    Added The Wordpress Loop

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
-<?php
-        wp_footer();
-?>
+    <?php
+    wp_footer();
+    ?>
+	    <footer class="footer text-center py-2 theme-bg-dark">
+		   
+           <p class="copyright"><a href="https://github.com/voidfire">Voidfire</a></p>
+          
+       </footer>
+   
+   </div>
 
 </body>
 </html> 
