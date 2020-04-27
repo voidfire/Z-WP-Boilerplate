dafa27d1017cb07081466884af3d20daaf6b283d
g.tsenis@gmail.com <g.tsenis@gmail.com>
2020-04-27 17:15:13 +0300

    Menus Refinements

--- header.php
+++ header.php
@@ -27,8 +27,22 @@
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
 				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
-				
-				<ul class="navbar-nav flex-column text-sm-center text-md-left">
+                
+                <?php
+                    wp_nav_menu(
+                        array(
+                            'menu' => 'primary',
+                            'container' => '',
+                            'theme_location' => 'primary',
+                            'items_wrap' => '<ul id="" class="navbar-nav flex-column text-sm-center text-md-left">%3$s</ul>',
+                        )
+                    );
+                ?>
+
+
+
+
+				<!-- <ul class="navbar-nav flex-column text-sm-center text-md-left">
 					<li class="nav-item active">
 					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
 					</li>
@@ -44,7 +58,8 @@
 					<li class="nav-item">
 					    <a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw mr-2"></i>Contact Us</a>
 					</li>
-				</ul>
+                </ul> -->
+                
 				<hr>
 				<ul class="social-list list-inline py-3 mx-auto">
 					<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
